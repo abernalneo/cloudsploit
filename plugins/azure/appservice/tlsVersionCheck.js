1c7d2be70f5aa707d7f60de91a9baf2178eec112
abernalneo <75950229+abernalneo@users.noreply.github.com>
2021-02-02 16:29:47 -0500

    Update tlsVersionCheck.js

--- plugins/azure/appservice/tlsVersionCheck.js
+++ plugins/azure/appservice/tlsVersionCheck.js
@@ -13,7 +13,7 @@ module.exports = {
         pci: 'PCI requires all web applications encrypt data ' +
             'in transit. This includes using the latest TLS ' +
             'version.',
-		cis1: '9.3 Ensure web app is using the latest version of TLS encryption'
+	cis1: '9.3 Ensure web app is using the latest version of TLS encryption'
     },
 
     run: function(cache, settings, callback) {
